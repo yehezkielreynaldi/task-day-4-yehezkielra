bf71ccc0538da57ef0be44195c8e8dafc04a5315
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-05-23 21:19:58 +0700

    third push / update become like a task day 5

--- js/project.js
+++ js/project.js
@@ -2,17 +2,46 @@
 
 let dataProject = [];
 
+
 function addProject(event) {
   event.preventDefault();
   let projectName = document.getElementById("input-project-name").value;
   let startDate = document.getElementById("input-project-start-date").value;
   let endDate = document.getElementById("input-project-end-date").value;
   let description = document.getElementById("input-project-description").value;
-  // let tech1 = document.getElementById("tech1").checked;
-  // let tech2 = document.getElementById("tech2").checked;
-  // let tech3 = document.getElementById("tech3").checked;
-  // let tech4 = document.getElementById("tech4").checked;
   let image = document.getElementById("input-project-image").files;
+
+  function getDifferenceTimeInput() {
+    let startDate = document.getElementById("input-project-start-date").value
+    let startToMilliseconds = new Date(startDate).getTime();
+    console.log(startToMilliseconds);
+    let endDate = document.getElementById("input-project-end-date").value;
+    let endToMilliseconds = new Date(endDate).getTime();
+    console.log(endToMilliseconds);
+    let selisih = endToMilliseconds - startToMilliseconds;
+    let durasiHari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+    console.log(durasiHari);
+    let durasiMinggu = Math.floor(selisih / (1000 * 60 * 60 * 24 * 7));
+    console.log(durasiMinggu);
+    let durasiBulan = Math.floor(selisih / (1000 * 60 * 60 * 24 * 7 * 4));
+    console.log(durasiBulan);
+    let durasiTahun = Math.floor(selisih / (1000 * 60 * 60 * 24 * 7 * 4 * 12));
+    console.log(durasiTahun);
+
+    // return `${durasiTahun} Tahun, ${durasiBulan} Bulan, ${durasiMinggu} Minggu, ${durasiHari} Hari`;
+
+    if (durasiTahun > 0) {
+      return `${durasiTahun} Tahun / ${durasiBulan} Bulan / ${durasiMinggu} Minggu / ${durasiHari} Hari`;
+    } else if (durasiBulan > 0) {
+      return `${durasiBulan} Bulan / ${durasiMinggu} Minggu / ${durasiHari} Hari`;
+    } if (durasiMinggu > 0) {
+      return `${durasiMinggu} Minggu / ${durasiHari} Hari`;
+    } if (durasiHari > 0) {
+      return `${durasiHari} Hari`
+    }
+  }
+
+  let duration = getDifferenceTimeInput();
 
   const nodeIcon = `<img src="assets/img/Node JS.png" alt="gambar-node" />`;
   const nextIcon = `<img src="assets/img/nextjs.png" alt="gambar-next" />`;
@@ -33,20 +62,21 @@ function addProject(event) {
     projectName,
     startDate,
     endDate,
+    duration,
     description,
     nodeIconCheck,
     nextIconCheck,
     reactIconCheck,
     typeIconCheck,
     image,
+    postAt: new Date(),
   };
 
   dataProject.push(project);
   console.log(dataProject);
 
-  renderProject()
+  renderProject();
 }
-
 
 
 function renderProject() {
@@ -66,7 +96,7 @@ function renderProject() {
                     >${dataProject[index].projectName}</a
                   >
                 </h5>
-                <p class="detail-img">durasi : ${dataProject[index].startDate} | ${dataProject[index].endDate}</p>
+                <p class="detail-img">durasi : ${dataProject[index].duration}</p>
                 <p class="caption">
                   ${dataProject[index].description}
                 </p>
@@ -88,6 +118,11 @@ function renderProject() {
                   <button class="btn-edit">edit</button>
                   <button class="btn-delete">delete</button>
                 </div>
+                <div style="float: right; margin: 0px">
+                <p style= "color: grey; font-size: 10px">
+                    ${getDistanceTime(dataProject[index].postAt)}
+                </p>
+            </div>
               </div>
             </article>
           </main>
@@ -117,3 +152,38 @@ function formWajibIsi() {
     return alert("Insert Your Project Image, Please ! ");
   }
 };
+
+function getDistanceTime(time) {
+  let timeNow = new Date();
+  let timePost = time;
+
+  // waktu sekarang - waktu post
+  let distance = timeNow - timePost; // hasilnya milidetik
+  console.log(distance);
+
+  let milisecond = 1000 // milisecond
+  let secondInHours = 3600 // 1 jam 3600 detik
+  let hoursInDays = 24 // 1 hari 24 jam
+
+  let distanceDay = Math.floor(distance / (milisecond * secondInHours * hoursInDays)); // 1/86400000
+  console.log(distanceDay);
+
+  let distanceHours = Math.floor(distance / (milisecond * 60 * 60)); // 1 / 3600000
+  let distanceMinutes = Math.floor(distance / (milisecond * 60)); // 1/60000
+  let distanceSecond = Math.floor(distance / milisecond); // 1/1000
+
+  if (distanceDay > 0) {
+    return `${distanceDay} Day Ago`;
+  } else if (distanceHours > 0) {
+    return `${distanceHours} Hours Ago`;
+  } else if (distanceMinutes > 0) {
+    return `${distanceMinutes} Minutes Ago`;
+  } else {
+    return `${distanceSecond} Seconds Ago`
+  }
+}
+
+// setInterval(function () {
+//   renderProject();
+// }, 2000);
+
